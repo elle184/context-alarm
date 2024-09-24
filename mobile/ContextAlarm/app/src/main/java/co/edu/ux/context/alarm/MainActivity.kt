3de20d9aab67cdede04a45e01872d16e2ede6cba
Adrián Francisco Becerra Arias <a.becerraa@uniandes.edu.co>
2024-09-23 22:22:40 -0500

    Se mejora el diseño del listado de alarmas.

--- mobile/ContextAlarm/app/src/main/java/co/edu/ux/context/alarm/MainActivity.kt
+++ mobile/ContextAlarm/app/src/main/java/co/edu/ux/context/alarm/MainActivity.kt
@@ -5,27 +5,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import co.edu.ux.context.alarm.data.Alarm
 import co.edu.ux.context.alarm.ui.theme.ContextAlarmTheme
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,26 +61,76 @@ fun ContextAlarm(modifier: Modifier = Modifier
 }
 
 @Composable
-fun Alarms(modifier: Modifier = Modifier
-    , alarms: List<String> = List(10) {"Alarma $it"}) {
-    Column {
-        Row {
-            Column(modifier = Modifier.weight(1f)
-                .padding(bottom = 8.dp)) {
-                Text("Vacio")
-            }
-            Text("Crear alarma")
-        }
-        LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-            items(items = alarms) { alarm ->
-                Greeting(name = alarm)
-            }
-        }
+fun AddImage(
+    image: Painter
+    , modifier: Modifier = Modifier) {
+    Box {
+        Image(painter = image, contentDescription = null
+            , modifier = modifier)
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun SwitchAlarmState() {
+    val switchChecked by remember { mutableStateOf(true) }
+
+    Switch(checked = true, onCheckedChange = null)
+}
+
+@Composable
+private fun NavigationBarContextAlarm(modifier: Modifier = Modifier) {
+    NavigationBar(modifier = modifier) {
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home
+                    , contentDescription = null)
+            }
+            , label = {
+                Text(
+                    text = "Alarmas"
+                )
+            }
+            , selected = true
+            , onClick = {}
+        )
+    }
+}
+
+@Composable
+fun Alarms(modifier: Modifier = Modifier) {
+    var alarms = mutableListOf(
+        Alarm("Alarma 1", "Ringtone 1", "L-M-V", "12:00")
+        , Alarm("Alarma 2", "Ringtone 2", "L-M-V", "12:00")
+        , Alarm("Alarma 3", "Ringtone 3", "L-M-V", "12:00")
+    )
+
+    Column {
+        Row(modifier = Modifier.padding(8.dp))  {
+            AddImage(
+                painterResource(id = R.drawable.context_alarm_logo)
+                , modifier = modifier)
+        }
+        Row(modifier = Modifier.padding(vertical = 8.dp, horizontal = 8.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)) {
+
+            }
+            AddImage(
+                painterResource(id = R.drawable.button_add)
+                , modifier = Modifier.size(50.dp))
+        }
+        LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+            items(items = alarms) { alarm ->
+                Greeting(alarm = alarm)
+            }
+        }
+
+    }
+}
+
+@Composable
+fun Greeting(alarm: Alarm, modifier: Modifier = Modifier) {
     val expanded = remember { mutableStateOf(false) }
     val extraPadding = if (expanded.value) 48.dp else 0.dp
 
@@ -78,11 +140,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
             Column(modifier = Modifier
                 .weight(1f)
                 .padding(bottom = extraPadding)) {
-                Text(text = "$name")
+                Text(text = alarm.name)
+                Text(text = alarm.ringtone)
+                Text(text = alarm.frequency)
             }
-            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(if (expanded.value) "Activar" else "Desactivado")
-            }
+            Text(text = alarm.hour)
+            SwitchAlarmState()
         }
     }
 }
@@ -96,6 +159,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ContextAlarmTheme {
-        ContextAlarm(modifier = Modifier.fillMaxSize())
+        Scaffold(
+            bottomBar = { NavigationBarContextAlarm() }
+        ) { padding ->
+            ContextAlarm(modifier = Modifier.padding(padding))
+        }
     }
 }
---
+++ mobile/ContextAlarm/app/src/main/java/co/edu/ux/context/alarm/data/Alarm.kt
@@ -0,0 +1,7 @@
+package co.edu.ux.context.alarm.data
+
+data class Alarm(
+    val name : String
+    , val ringtone : String
+    , val frequency : String
+    , val hour : String)
